71ccb79374c133a6ac6679b495adedd0cef1a0a9
marivgil <gil.maricruz@gmail.com>
2018-09-23 19:50:21 -0300

    Se agregan servicios rest de hospitales, dar de alta un producto y consultar listado de productos por hospital

---
+++ src/main/java/persistence/repositories/HospitalProductRepository.java
@@ -0,0 +1,34 @@
+package persistence.repositories;
+
+import model.HospitalProduct;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.orm.hibernate4.HibernateCallback;
+
+import java.util.List;
+
+public class HospitalProductRepository
+        extends HibernateGenericDAO<HospitalProduct>
+        implements GenericRepository<HospitalProduct> {
+
+    @Override
+    protected Class<HospitalProduct> getDomainClass() {
+        return HospitalProduct.class;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public List<HospitalProduct> findByHospital(String hospital) {
+        return (List<HospitalProduct>) this.getHibernateTemplate().execute(new HibernateCallback() {
+            @Override
+            public List<HospitalProduct> doInHibernate(final Session session) throws HibernateException {
+                Criteria criteria = session.createCriteria(HospitalProduct.class, "product")
+                        .add(Restrictions.eq("hospital", hospital ))
+                        ;
+                return (List<HospitalProduct>)criteria.list();
+            }
+
+        });
+    }
+}
